13586ba7987b20c2ecc331ea6a89790e484e00f4
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-03-03 16:37:56 -0600

    adds lesson 7

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+Vue.component('task-list', {
+
+    template:`
+        <div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div>
+    `,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the bank', completed: false },
+                { task: 'Go to the store', completed: false },
+                { task: 'Go on a date', completed: false },
+                { task: 'Drink coffee', completed: false },
+                { task: 'Code', completed: false }
+            ]
+        }
+    }
+
+})
+
 Vue.component('task', {
     template: '<li><slot></slot></li>',
-    
 })
 
 new Vue({
     el:'#root',
-
 })
